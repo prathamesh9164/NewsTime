04c00f3631e42d76590974d78243300361dd06e8
prathamesh9164 <prathameshtalele1@gmail.com>
2025-04-03 08:30:23 +0530

    hb

---
+++ src/components/NewsItem.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date } = this.props;
+    return (
+      <div>
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://media.istockphoto.com/id/1409309637/vector/breaking-news-label-banner-isolated-vector-design.jpg?s=612x612&w=0&k=20&c=JoQHezk8t4hw8xXR1_DtTeWELoUzroAevPHo0Lth2Ow="
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By {!author?"Unknown": author} on {date}</small></p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
